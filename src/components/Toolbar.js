88b09474fea2a742ed12ed61563a41d8fb722302
Lucian Carp <lucian.g.carp@gmail.com>
2021-06-10 15:14:54 +0300

    Implement Toolbar checkbox status

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,37 +1,59 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
 
 import { connect } from 'react-redux'
 import { selectAllFiles, deselectAllFiles } from '../actions/file'
 
+import Checkbox from './Checkbox'
+
 const Toolbar = ({
   selectAllFiles,
   deselectAllFiles,
-  file: { selectedFile, files },
+  file: { selectedFiles, files },
 }) => {
+  const [status, setStatus] = useState(-1)
+
   useEffect(() => {
-    checkboxRef.current.checked = true
-    checkboxRef.current.indeterminate = true
-  }, [])
+    const getCheckboxStatus = (selected, all) => {
+      let status = 1
 
-  let checked = false
+      if (selected.length === 0) return -1
 
-  const checkboxRef = React.useRef()
+      for (let i = 0; i < all.length; i++) {
+        if (!selected.includes(all[i])) {
+          status = 0
+          break
+        }
+      }
+
+      return status
+    }
+
+    setStatus(
+      getCheckboxStatus(
+        selectedFiles,
+        files.map((file) => file.id)
+      )
+    )
+  }, [selectedFiles, files])
+
+  const handleChange = () => {
+    if (status === -1 || status === 0) {
+      selectAllFiles()
+    } else {
+      deselectAllFiles()
+    }
+  }
 
   return (
     <Container>
-      <StyledCheckbox type='checkbox' id='box' ref={checkboxRef} />
-      {/* <label for='box'>-</label> */}
+      <Checkbox status={status} onChange={() => handleChange()} />
       <h3>Selected</h3>
       <h3>Download</h3>
     </Container>
   )
 }
-
-const StyledCheckbox = styled.input`
-  /* display: none; */
-`
 
 const Container = styled.div``
 
